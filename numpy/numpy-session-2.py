492a2d8097a3d9fe6603f14f33a748245c9da12b
hoodielive <hoodielive@gmail.com>
2019-06-16 16:36:43 -0400

    arithmetic with numpy

---
+++ numpy/numpy-session-2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+list1 = [[1,2,3], [4,5,6]]
+list2 = [[7,8,9], [10,11,12]]
+
+arr1 = np.array(list1)
+arr2 = np.array(list2)
+
+print(arr1)
+print(arr2)
+
+print(arr1 + arr2)
+print(arr1 - arr2)
+print(arr1 * arr2)
+print(arr1 / arr2)
+
+print(arr1 * 10)
+print(arr1 / 10)
