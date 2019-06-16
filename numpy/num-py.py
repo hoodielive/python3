f5f2c1c6ede4f3e6412510187fb6203f4c0f8ad6
hoodielive <hoodielive@gmail.com>
2019-06-16 16:30:53 -0400

    coercions

--- numpy/num-py.py
+++ numpy/num-py.py
@@ -28,3 +28,16 @@ print(arr)
 
 arr = np.zeros((2,3))
 print(arr)
+
+# create floating point arrays
+arr = np.array([1,2,3,4,5], dtype=np.float64)
+print(arr)
+
+# create integers from floats
+arr = arr.astype(np.int64)
+print(arr)
+
+arr = np.array([1.3, 2.4, 5.8, 9.6])
+print(arr)
+
+print(arr.astype(np.int64))
