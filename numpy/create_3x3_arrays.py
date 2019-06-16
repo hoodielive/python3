28dd4b2fe1c9d346085dd3345d70d137bc82d77e
hoodielive <hoodielive@gmail.com>
2019-06-16 17:14:49 -0400

    create 3x3 arrays and add them

---
+++ numpy/create_3x3_arrays.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+list1 = [[1,2,3], [4,5,6], [7,8,9]]
+list2 = [[10,11,12], [13,14,15], [16,17,18]]
+
+arr1 = np.array(list1)
+arr2= np.array(list2)
+print(arr1 + arr2)
+
+list3 = [[1,2,3], [4,5,6], [7,8,9]]
+arr3 = np.array(list3)
+
+print(arr3 * 5)
---
+++ numpy/two_dimensional_array.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+two_dim = [[1,2,3],[4,5,6]]
+
+arr = np.array(two_dim)
+print(arr)
